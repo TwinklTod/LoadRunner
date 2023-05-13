94387407b24c2bd1048a60aa3c27a2deac0f78ee
Eugene <skornevgen@gmail.com>
2023-05-14 01:15:23 +0500

    Исправление комментариев

--- Scripts/UC_2_Delete_booking/Action.c
+++ Scripts/UC_2_Delete_booking/Action.c
@@ -10,9 +10,21 @@ Action()
 		"Text/IC=<H3>No flights have been reserved.</H3>",
 		LAST);
 
+	web_reg_save_param("flightID",
+		"LB/IC=flightID\" value=\"",
+		"RB/IC=\"",
+		"Ord=ALL",
+		LAST);
+	
+	web_reg_save_param("checkbox",
+		"LB/IC=checkbox\" name=\"",
+		"RB/IC=\"",
+		"Ord=ALL",
+		LAST);
+		
 	click_itinerary();
-
-	                
+	lr_save_string(lr_paramarr_random("checkbox"), "rnd_checkbox");
+		                
 	if (atoi(lr_eval_string("{NoBooking}")) == 1) {
 	    logout();
 	    }
@@ -20,14 +32,20 @@ Action()
 	 			
 	lr_start_transaction("cancel_booking");
 	
+	
+	
 	web_reg_find("Fail=Found",
-		"Text/IC={flightID}",
+		"Text/IC={rnd_flightID}",
 		LAST);
+	
+	lr_save_string(
+		lr_paramarr_idx("flightID", atoi(lr_paramarr_random("checkbox"))),
+		"rnd_flightID");
 	
 	web_submit_form("itinerary.pl", 
 		"Snapshot=t4.inf", 
 		ITEMDATA, 
-		"Name=1", "Value=on", ENDITEM,
+		"Name={rnd_checkbox}", "Value=on", ENDITEM,
 		"Name=removeFlights.x", "Value=55", ENDITEM,
 		"Name=removeFlights.y", "Value=9", ENDITEM, 
 		LAST);
